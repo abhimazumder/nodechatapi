388c531148cfd0fb3429ffa72162bdcb0d0ff262
Abhishek Mazumder <abhishek.am988@gmail.com>
2023-06-08 04:57:32 +0530

    Partially implemented /messages

--- message/messages.js
+++ message/messages.js
@@ -2,8 +2,10 @@
 
 const AWS = require('aws-sdk');
 const { isEmail } = require('validator');
+const { checkAuth } = require('../utils/checkAuth');
 
 const documentClient = new AWS.DynamoDB.DocumentClient();
+const s3 = new AWS.S3();
 
 module.exports.handler = async (event) => {
     try {
@@ -19,6 +21,18 @@ module.exports.handler = async (event) => {
 
         const data = await documentClient.scan(params).promise();
 
+        for (const message of data.Items) {
+            const fileExtension = message.content.split('.').pop();
+            if (fileExtension.toLowerCase() === 'txt') {
+                const params = {
+                    Bucket: 'nodechatapi-dev-mys3bucket2-1dyh810yatk7',
+                    Key: message.content,
+                };
+                const data = await s3.getObject(params).promise();
+                message.content = data.Body.toString('utf-8');
+            }
+        }
+
         return {
             statusCode: 200,
             headers: {
@@ -26,10 +40,10 @@ module.exports.handler = async (event) => {
                 "Access-Control-Allow-Headers": "Content-Type",
                 "Access-Control-Allow-Methods": "GET",
             },
-            body : JSON.stringify(data.Items),
+            body: JSON.stringify(data.Items),
         };
     }
-    catch(err){
+    catch (err) {
         return {
             statusCode: err.statusCode || 500,
             headers: {
